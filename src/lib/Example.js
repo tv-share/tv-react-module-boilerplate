22ac84393088521f6218243604138223b744185e
Thiago <thiago@thevelops.com>
2018-02-23 01:25:03 -0300

    added build dependencies

--- src/lib/Example.js
+++ src/lib/Example.js
@@ -2,18 +2,18 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Example = (props) => {
-    const { className, label, onClick } = props;
+	const { className, label, onClick } = props;
 	return (
 		<button className={`example${className ? className : ""}`} onClick={onClick}>
-            {label || ""}
+			{label || ""}
 		</button>
 	);
 };
 
 Example.propTypes = {
-    label: PropTypes.string.isRequired,
-    className: PropTypes.string,
-    onClick: PropTypes.func
+	label: PropTypes.string.isRequired,
+	className: PropTypes.string,
+	onClick: PropTypes.func
 };
 
 export default Example;
